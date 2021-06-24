2dc4db7584d9646d4ef2b9cd68966cdad9d51950
thehaigo <sirannha@gmail.com>
2021-06-24 23:20:18 +0900

    add token to users

--- lib/live_map/accounts/user.ex
+++ lib/live_map/accounts/user.ex
@@ -8,6 +8,7 @@ defmodule LiveMap.Accounts.User do
     field :password, :string, virtual: true
     field :hashed_password, :string
     field :confirmed_at, :naive_datetime
+    field :token, :string
 
     timestamps()
   end
