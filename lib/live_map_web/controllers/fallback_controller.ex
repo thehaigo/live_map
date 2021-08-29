5b81e970931a40789feabb0c2653684b951e7a05
thehaigo <sirannha@gmail.com>
2021-08-29 21:36:32 +0900

    fallback controller add

---
+++ lib/live_map_web/controllers/fallback_controller.ex
@@ -0,0 +1,35 @@
+defmodule LiveMapWeb.FallbackController do
+  @moduledoc """
+  Translates controller action results into valid `Plug.Conn` responses.
+
+  See `Phoenix.Controller.action_fallback/1` for more details.
+  """
+  use LiveMapWeb, :controller
+
+  def call(conn, {:error, %Ecto.Changeset{} = changeset}) do
+    conn
+    |> put_status(:unprocessable_entity)
+    |> put_view(LiveMapWeb.ChangesetView)
+    |> render("error.json", changeset: changeset)
+  end
+
+  def call(conn, {:error, :not_found}) do
+    conn
+    |> put_status(:not_found)
+    |> put_view(LiveMapWeb.ErrorView)
+    |> render(:"404")
+  end
+
+  def call(conn, {:error, :unauthorized}) do
+    conn
+    |> put_status(:unauthorized)
+    |> json(%{error: "Login error"})
+  end
+
+  def call(conn, {:error, _param}) do
+    conn
+    |> put_status(:internal_server_error)
+    |> put_view(LiveMapWeb.ErrorView)
+    |> render(:"500")
+  end
+end
