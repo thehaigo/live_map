a61fcfcf890fa1208f084ea6bf385f6b69d4d4c7
thehaigo <sirannha@gmail.com>
2021-10-13 15:29:01 +0900

    modify presence util

--- lib/live_map_web/channels/presence.ex
+++ lib/live_map_web/channels/presence.ex
@@ -5,18 +5,15 @@ defmodule LiveMapWeb.Presence do
   See the [`Phoenix.Presence`](http://hexdocs.pm/phoenix/Phoenix.Presence.html)
   docs for more details.
   """
-  use Phoenix.Presence, otp_app: :live_map,
-                        pubsub_server: LiveMap.PubSub
+  use Phoenix.Presence,
+    otp_app: :live_map,
+    pubsub_server: LiveMap.PubSub
 
   alias LiveMapWeb.Presence
 
-  def track_presence(pid, topic, key, payload) do
-    Presence.track(pid, topic, key, payload)
-  end
-
   def list_presence(topic) do
     topic
-    |> Presence.list
+    |> Presence.list()
     |> Enum.map(fn {_user_id, data} -> extract_metadata(data) end)
   end
 
@@ -29,18 +26,20 @@ defmodule LiveMapWeb.Presence do
     Presence.update(pid, topic, key, metas)
   end
 
-  def update_all_presence(pid,topic,payload) do
+  def update_all_presence(pid, topic, payload) do
     topic
-    |> Presence.list
+    |> Presence.list()
     |> Enum.each(fn {key, _data} -> update_presence(pid, topic, key, payload) end)
   end
 
-  def get_paresence(topic, key) do
+  def get_presence(topic, key) do
     Presence.get_by_key(topic, key)
     |> extract_metadata
   end
 
-  def extract_metadata(data) do
-    data |> Map.get(:metas) |> List.first
+  def extract_metadata(data) when is_list(data), do: nil
+
+  def extract_metadata(data) when is_map(data) do
+    data |> Map.get(:metas) |> List.first()
   end
 end
