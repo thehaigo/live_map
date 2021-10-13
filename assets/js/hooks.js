36bf18690fc3b0b1a4bb732ada13d88f6facdcf9
thehaigo <sirannha@gmail.com>
2021-10-13 15:34:00 +0900

    tracking, view route log update

--- assets/js/hooks.js
+++ assets/js/hooks.js
@@ -3,59 +3,80 @@ let Hooks = {};
 
 Hooks.Map = {
   mounted() {
+    const loader = new Loader({
+      apiKey: "your google map api key",
+      version: "weekly",
+      language: "en",
+    });
     this.handleEvent("init_map", ({ point, devices }) => {
-      const loader = new Loader({
-        apiKey: "your google map api key",
-        version: "weekly",
-      });
       loader.load().then(() => {
-        const center = !point ?
-          { lat: 33.30639, lng: 130.41806 }
-          :
-          { lat: point.lat, lng: point.lng }
+        const center = !point
+          ? { lat: 33.30639, lng: 130.41806 }
+          : { lat: point.lat, lng: point.lng };
 
         const map = new google.maps.Map(document.getElementById("map"), {
           center: center,
           zoom: 12,
         });
         window.map = map;
-        devices.forEach(device => window.map.data.addGeoJson(device))
+        window.markers = [];
+
+        devices.forEach((point) => {
+          let marker = new google.maps.Marker({
+            position: { lat: point.lat, lng: point.lng },
+          });
+          marker.setMap(window.map);
+          window.markers = [
+            ...window.markers,
+            { uuid: point.uuid, marker: marker },
+          ];
+        });
       });
     });
 
-    this.handleEvent("location_update", ({ uuid, latlng, point, points, viewing,tracking }) => {
-      window.map.data.forEach(device => {
-        if(device.i.name == `${uuid}Point`) {
-          window.map.data.remove(device)
-          window.map.data.addGeoJson(point)
+    this.handleEvent(
+      "location_update",
+      ({ uuid, latlng, points, viewing, tracking }) => {
+        if (tracking) {
+          window.map.setCenter(latlng);
         }
-        if(viewing && device.i.name == `${uuid}LineString`) {
-          window.map.data.remove(device)
-          window.map.data.addGeoJson(points)
-        }
-        if(tracking) {
-          window.map.setCenter(latlng)
-        }
-      })
 
-    });
+        const device = window.markers.find((m) => m.uuid == uuid);
+        if (device) {
+          device.marker.setPosition(latlng);
+        } else {
+          const marker = new google.maps.Marker({
+            position: latlng,
+            animation: google.maps.Animation.DROP,
+          });
+          marker.setMap(window.map);
+          window.markers = [...window.markers, { uuid: uuid, marker: marker }];
+        }
+        window.map.data.forEach((device) => {
+          if (viewing && device.i.name == `${uuid}LineString`) {
+            window.map.data.remove(device);
+            window.map.data.addGeoJson(points);
+          }
+        });
+      }
+    );
 
-    this.handleEvent("track_device",({ point }) => {
-      window.map.setCenter({ lat: point.lat, lng: point.lng })
+    this.handleEvent("track_device", ({ point }) => {
+      window.map.setCenter({ lat: point.lat, lng: point.lng });
     });
 
     this.handleEvent("view_device_log", ({ uuid, json, viewing }) => {
-      if(viewing) {
-        window.map.data.addGeoJson(json)
+      if (viewing) {
+        window.map.data.addGeoJson(json);
       } else {
-        window.map.data.forEach(device => {
-          if(device.i.name == `${uuid}LineString`) {
-            window.map.data.remove(device)
+        window.map.data.forEach((device) => {
+          if (device.i.name == `${uuid}LineString`) {
+            window.map.data.remove(device);
           }
-        })
+        });
       }
-    })
-  }
+    });
+  },
 };
 
 export default Hooks;
--- lib/live_map_web/live/map_live/show.ex
+++ lib/live_map_web/live/map_live/show.ex
@@ -6,77 +6,70 @@ defmodule LiveMapWeb.MapLive.Show do
 
   @impl true
   def mount(%{"id" => id}, _session, socket) do
+    topic = "map:#{id}"
     Loggers.subscribe({:map, id})
     map = Loggers.get_map_with_points!(id)
     center = map.points |> Enum.take(1) |> Enum.map(fn p -> %{lat: p.lat, lng: p.lng} end)
-    devices = points_to_group_by_devices(map.points)
-    # markerはgeo jsonではなく window.markers で管理する
+
+    if Presence.list(topic) |> map_size() == 0 do
+      map.points
+      |> points_to_group_by_devices()
+      |> track_all_devices(id)
+    end
+
+    devices = Presence.list_presence(topic)
+
     markers =
-      devices
-      |> Enum.map(fn d ->
-        d |>
-        Map.put(
-          :points,
-          Enum.take(d.points,1)
-          |> List.first
-        )
-        |> generate_geo_json("Point")
+      Enum.map(devices, fn d ->
+        %{uuid: d.uuid, lat: d.current_location.lat, lng: d.current_location.lng}
       end)
-    track_all_devices(devices, map)
+
     {
       :ok,
       socket
       |> assign(:page_title, page_title(socket.assigns.live_action))
       |> assign(:map, map)
       |> assign(:devices, Presence.list_presence("map:#{map.id}"))
-      |> push_event("init_map",%{point: List.first(center), devices: markers})
+      |> push_event("init_map", %{point: List.first(center), devices: markers})
     }
   end
 
   @impl true
   def handle_info({:created_point, point}, socket) do
-    topic = "map:#{socket.assigns.map.id}"
-    device = Presence.get_paresence(topic, point.device_id)
-    points = [[point.lng, point.lat]|device.points]
-    Presence.update_presence(
-      self(),
-      topic,
-      point.device_id,
-      %{
-        current_location: point,
-        points: points
-      })
+    create_or_update_marker(point)
+    device = Presence.get_presence("map:#{point.map_id}", point.device_id)
+
     {
       :noreply,
       socket
-      |> push_event("location_update",
+      |> push_event(
+        "location_update",
         %{
-          uuid: point.device_id,
-          latlng: %{lat: point.lat,lng: point.lng},
-          point:
-            %{uuid: point.device_id, points: [point.lng, point.lat]}
-            |> generate_geo_json("Point"),
-          points: %{uuid: point.device_id, points: points} |> generate_geo_json("LineString"),
+          uuid: device.uuid,
+          latlng: %{lat: point.lat, lng: point.lng},
+          points: %{uuid: device.uuid, points: device.points} |> generate_geo_json("LineString"),
           viewing: device.viewing,
           tracking: device.tracking
-        })
+        }
+      )
     }
   end
 
   @impl true
-  def handle_info(%{event: "presence_diff"}, socket = %{ assigns: %{map: map}}) do
+  def handle_info(%{event: "presence_diff"}, socket = %{assigns: %{map: map}}) do
     {:noreply, assign(socket, devices: Presence.list_presence("map:#{map.id}"))}
   end
 
   @impl true
   def handle_event("track", %{"uuid" => uuid}, socket) do
     topic = "map:#{socket.assigns.map.id}"
-    Presence.update_all_presence(self(),topic, %{tracking: false})
+    Presence.update_all_presence(self(), topic, %{tracking: false})
     Presence.update_presence(self(), topic, uuid, %{tracking: true})
-    device = Presence.get_paresence(topic,uuid)
+    device = Presence.get_presence(topic, uuid)
+
     {
       :noreply,
-      push_event(socket,"track_device", %{
+      push_event(socket, "track_device", %{
         point: %{
           lat: device.current_location.lat,
           lng: device.current_location.lng
@@ -88,7 +81,7 @@ defmodule LiveMapWeb.MapLive.Show do
   @impl true
   def handle_event("viewing", %{"uuid" => uuid}, socket) do
     topic = "map:#{socket.assigns.map.id}"
-    device = Presence.get_paresence(topic, uuid)
+    device = Presence.get_presence(topic, uuid)
     Presence.update_presence(self(), topic, uuid, %{viewing: !device.viewing})
 
     {
@@ -109,41 +102,70 @@ defmodule LiveMapWeb.MapLive.Show do
   defp page_title(:edit), do: "Edit Map"
 
   defp points_to_group_by_devices(points) do
-    Enum.group_by(points, fn p -> { p.device_id, p.user_agent} end)
-    |> Map.to_list()
+    Enum.group_by(points, fn p -> {p.device_id, p.user_agent} end)
     |> Enum.map(fn {{device_id, user_agent}, logs} ->
-        %{
-          uuid: device_id,
-          ua: user_agent,
-          current: List.first(logs),
-          points: Enum.map(logs, fn p -> [p.lng,p.lat] end)
-        }
-      end)
+      %{
+        uuid: device_id,
+        ua: user_agent,
+        current: List.first(logs),
+        points: logs |> Enum.map(fn p -> [p.lng, p.lat] end)
+      }
+    end)
   end
 
   defp generate_geo_json(device, type) do
     %{
-        type: "Feature",
-        properties: %{ name: device.uuid <> type },
-        geometry: %{type: type, coordinates: device.points}
+      type: "Feature",
+      properties: %{name: device.uuid <> type},
+      geometry: %{type: type, coordinates: device.points}
     }
   end
 
-  defp track_all_devices(devices, map) do
+  defp track_all_devices(devices, id) do
     Enum.each(devices, fn device ->
-      Presence.track_presence(
-        self(),
-        "map:#{map.id}",
-        device.uuid,
-        %{
-          name: device.ua,
-          uuid: device.uuid,
-          current_location: device.current,
-          tracking: false,
-          viewing: false,
-          points: device.points
-        }
-      )
+      track(device, id)
     end)
+  end
+
+  defp track(device, id) do
+    Presence.track(
+      self(),
+      "map:#{id}",
+      device.uuid,
+      %{
+        name: device.ua,
+        uuid: device.uuid,
+        current_location: device.current,
+        tracking: false,
+        viewing: false,
+        points: device.points
+      }
+    )
+  end
+
+  defp create_or_update_marker(point) do
+    topic = "map:#{point.map_id}"
+    device = Presence.get_presence(topic, point.device_id)
+
+    if device do
+      points = [[point.lng, point.lat] | device.points]
+
+      Presence.update_presence(
+        self(),
+        topic,
+        point.device_id,
+        %{current_location: point, points: points}
+      )
+    else
+      track(
+        %{
+          uuid: point.device_id,
+          ua: point.user_agent,
+          current: point,
+          points: [[point.lng, point.lat]]
+        },
+        point.map_id
+      )
+    end
   end
 end
